995ff463b256bc1467466e96b3883981709635f1
KerryanOPMace <kerryan.mace@student-cs.fr>
2025-11-09 11:34:48 +0100

    Nettoyage de la pipeline

--- main.py
+++ main.py
@@ -204,4 +204,4 @@ def process_items_list(articles: List[Dict], store: str, city: str = "Le port-ma
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
